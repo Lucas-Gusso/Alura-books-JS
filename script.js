ffb532eb087734aa8045aeb4e7f951a9a0b99212
Lucas <lucasgusso28@gmail.com>
2023-07-31 15:40:04 -0300

    Aula 4 - vídeo 1

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("oi");
+async function buscaEndereco() {
+    let consultaCEP = await fetch('https://viacep.com.br/ws/13024430/json/')
+    let consultaCEPConvertida = await consultaCEP.json();
+
+    console.log(consultaCEPConvertida);
+}
+
+
+
+
+buscaEndereco();
