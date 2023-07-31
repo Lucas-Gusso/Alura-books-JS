0830eb38e323d48e80ee4207d50a61fcdc0e2814
Lucas <lucasgusso28@gmail.com>
2023-07-31 16:05:59 -0300

    Aula 4 - vídeo 3

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-async function buscaEndereco() {
+async function buscaEndereco(cep) {
     try {
 
-    let consultaCEP = await fetch('https://viacep.com.br/ws/13004400/json/')
+    let consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
     let consultaCEPConvertida = await consultaCEP.json();
         if (consultaCEPConvertida.erro) {
             throw Error('CEP não existente!');
         }
     console.log(consultaCEPConvertida);
+    return consultaCEPConvertida;
 
     } catch (e) {
         console.log(e);
@@ -15,5 +16,7 @@ async function buscaEndereco() {
 
 
 
-
-buscaEndereco();
+let ceps = ['13024430','01001000'];
+let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
+console.log(conjuntoCeps)
+Promise.all(conjuntoCeps).then(r => console.log(r));
